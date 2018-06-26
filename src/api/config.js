830a7aebd2f0555aabac1516f65b399740de93c4
Mauricio Guarda <fifomakenmen7000@gmail.com>
2018-06-25 23:37:04 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+    appName: 'platzimusic',
+    apiKey: '483927b661f8bb79022899f3c6ee4c2a',
+    secret: 'db6cb1fe991390e7f876aacdd40a6b33',
+    registeredTo: 'Makenmen' 
+}
+
+// Application name	platzimusic
+// API key	483927b661f8bb79022899f3c6ee4c2a
+// Shared secret	db6cb1fe991390e7f876aacdd40a6b33
+// Registered to	Makenmen
